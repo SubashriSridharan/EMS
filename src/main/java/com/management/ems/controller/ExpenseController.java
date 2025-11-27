9dc513f6ebdc8501fba730398b72ad481e0b1771
Subskrish <subasridharan0@gmail.com>
2025-11-27 18:07:05 +0530

    Log data

--- src/main/java/com/management/ems/controller/ExpenseController.java
+++ src/main/java/com/management/ems/controller/ExpenseController.java
@@ -20,6 +20,7 @@ public class ExpenseController {
 
     @PostMapping("/")
     public ResponseEntity<ResponseDto> addExpense(@RequestBody ExpenseRequestDto expenseRequestDto){
+        System.out.println("Expense Controller Data");
         return expenseService.addExpense(expenseRequestDto);
     }
 }
